e6e735d0b61964bd37fabbeb389af8c8df510b39
Mei Huang <meihen0922@gmail.com>
2025-02-18 14:20:02 +0800

    feat: 實現 mini-saga effects

---
+++ mini/saga/effects.ts
@@ -0,0 +1,40 @@
+import { IO } from "./symbol";
+
+const effectTypes = {
+  TAKE: "TAKE",
+  PUT: "PUT",
+  ALL: "ALL",
+  RACE: "RACE",
+  CALL: "CALL",
+  CPS: "CPS",
+  FORK: "FORK",
+  JOIN: "JOIN",
+  CANCEL: "CANCEL",
+  SELECT: "SELECT",
+  ACTION_CHANNEL: "ACTION_CHANNEL",
+  CANCELLED: "CANCELLED",
+  FLUSH: "FLUSH",
+  GET_CONTEXT: "GET_CONTEXT",
+  SET_CONTEXT: "SET_CONTEXT",
+} as const;
+
+function makeEffect(type: keyof typeof effectTypes, payload) {
+  return {
+    type,
+    payload,
+    [IO]: IO,
+  };
+}
+
+export function take(pattern) {
+  return makeEffect(effectTypes.TAKE, { pattern });
+}
+export function put(action) {
+  return makeEffect(effectTypes.PUT, { action });
+}
+export function call(fn, ...args) {
+  return makeEffect(effectTypes.CALL, { fn, args });
+}
+export function fork(fn, ...args) {
+  return makeEffect(effectTypes.FORK, { fn, args });
+}
